a3ff208dff50a3960b0a8a7c2d43f6b809830346
Anton <reopik@mail.ru>
2023-08-11 16:13:26 +0300

    made header html markup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+        <svg />
+        <div>
+          <h3>React Sneakers</h3>
+          <p>Магазин лучших кроссовок</p>
+        </div>
+        <ul>
+          <li>
+            <svg />
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <svg />
+          </li>
+        </ul>
       </header>
     </div>
   );
